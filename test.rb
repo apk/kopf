2e3c189adfe7ab878ff37e307aae9105f21d71dc
Andreas Krey <a.krey@gmx.de>
2016-04-19 23:14:15 +0200

    test: selectable config

--- test.rb
+++ test.rb
@@ -3,9 +3,17 @@ require_relative 'src/jobset.rb'
 js=JobSet.new(false)
 ps=JobSet.new(true)
 
+cfgfile='test.cfg'
+
+ARGV.each do |a|
+  cfgfile=a
+end
+
+puts "Config: #{cfgfile}"
+
 while true do
   begin
-    cfg=JSON.parse(File.read('test.cfg'))
+    cfg=JSON.parse(File.read(cfgfile))
     ps.load_json(cfg['procs'])
     js.load_json(cfg['jobs'])
   rescue => e
