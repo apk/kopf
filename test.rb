6c70302d53778dfee8b82c4a50d35a6ce2767a9e
Andreas Krey <a.krey@gmx.de>
2016-04-20 17:50:08 +0200

    test.rb: add #diag, chdir to config file, allow requires

--- test.rb
+++ test.rb
@@ -1,7 +1,13 @@
 require_relative 'src/jobset.rb'
 
+def diag(s)
+  STDERR.puts "    #{Time.now.to_s}: #{s}"
+end
+
 js=JobSet.new(false)
 ps=JobSet.new(true)
+
+$jobset=js # Dirty, but for now for the plugins
 
 cfgfile='test.cfg'
 
@@ -9,20 +15,47 @@ ARGV.each do |a|
   cfgfile=a
 end
 
+cfgdir='.'
+if cfgfile =~ /\/([^\/]+)/
+  cfgdir=$'
+  cfgfile=$1
+end
+
+Dir.chdir cfgdir
+
 puts "Config: #{cfgfile}"
 
-while true do
+tm=nil
+while true
   begin
-    cfg=JSON.parse(File.read(cfgfile))
-    ps.load_json(cfg['procs'])
-    js.load_json(cfg['jobs'])
+    s=File.stat(cfgfile)
+    if s.mtime != tm
+      diag "Config #{cfgfile.inspect} changed..."
+      tm=s.mtime
+      cfg=JSON.parse(File.read(cfgfile))
+      ps.load_json(cfg['procs'])
+      js.load_json(cfg['jobs'])
+
+      reqs=cfg['requires']
+      if reqs
+        if reqs.is_a? String
+          reqs=[reqs]
+        end
+        if reqs.is_a? Array
+          reqs.each do |r|
+            if r.is_a? String
+              diag "Load: #{r.inspect}"
+              require './'+r
+            end
+          end
+        end
+      end
+    end
   rescue => e
     puts "E: #{e.inspect}"
     e.backtrace.each do |b|
       puts ":: #{b}"
     end
   end
-
-  sleep (10)
-
+  sleep 10
 end
