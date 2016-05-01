aeb6032fa839cf0adfe9c629c5e210a07b7f5bf1
Andreas Krey <a.krey@gmx.de>
2016-05-01 06:17:06 +0200

    combine: create executable script

--- combine.sh
+++ combine.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-cd "`dirname "$0"`"
-cat src/mailer.rb src/cron.rb src/cfg.rb src/jobset.rb src/runner.rb test.rb | grep -v require_relative
+q="`dirname "$0"`"
+(if test -x /opt/rh-ng/ruby-200/root/usr/bin/ruby; then
+	echo '#!/opt/rh-ng/ruby-200/root/usr/bin/ruby'
+ else
+	echo '#!/usr/bin/ruby'
+ fi
+ cat "$q"/src/mailer.rb "$q"/src/cron.rb "$q"/src/cfg.rb "$q"/src/jobset.rb "$q"/src/runner.rb "$q"/test.rb | grep -v require_relative
+) >kopf
+chmod +x kopf
