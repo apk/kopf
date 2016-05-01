db8a87fa249558b72e1492836b50d117e431f08a
Andreas Krey <a.krey@gmx.de>
2016-05-01 06:51:08 +0200

    combine: keep existing #!, add commit id

--- combine.sh
+++ combine.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 q="`dirname "$0"`"
-(if test -x /opt/rh-ng/ruby-200/root/usr/bin/ruby; then
-	echo '#!/opt/rh-ng/ruby-200/root/usr/bin/ruby'
- else
-	echo '#!/usr/bin/ruby'
- fi
- cat "$q"/src/mailer.rb "$q"/src/cron.rb "$q"/src/cfg.rb "$q"/src/jobset.rb "$q"/src/runner.rb "$q"/test.rb | grep -v require_relative
+head=''
+if test -r kopf; then
+    # Try to keep original hashbang if present
+    head="`head -1 kopf | grep '^#!'`"
+fi
+if test "X$head" == X; then
+    head='#!/usr/bin/ruby'
+fi
+(
+    echo "$head"
+    echo "#"
+    echo "# kopf `git -C "$q" describe --always`"
+    echo "#"
+    cat \
+	"$q"/src/mailer.rb \
+	"$q"/src/cron.rb \
+	"$q"/src/cfg.rb \
+	"$q"/src/jobset.rb \
+	"$q"/src/runner.rb \
+	"$q"/test.rb | grep -v require_relative
 ) >kopf
 chmod +x kopf
