de96e6ef7b47359d4082190703a521684bdd5082
Andreas Krey <a.krey@gmx.de>
2017-04-21 06:52:54 +0200

    combine: update kopf-msgclt.rb when present

--- combine.sh
+++ combine.sh
@@ -5,7 +5,7 @@ if test -r kopf; then
     # Try to keep original hashbang if present
     head="`head -1 kopf | grep '^#!'`"
 fi
-if test "X$head" == X; then
+if test "X$head" = X; then
     head='#!/usr/bin/ruby'
 fi
 (
@@ -22,3 +22,12 @@ fi
 	"$q"/test.rb | grep -v require_relative
 ) >kopf
 chmod +x kopf
+
+if test -r kopf-msgclt.rb; then
+    (
+	echo "#"
+	echo "# kopf `git -C "$q" describe --always`"
+	echo "#"
+	cat "$q"/kopf-msgclt.rb
+    ) >kopf-msgclt.rb
+fi
